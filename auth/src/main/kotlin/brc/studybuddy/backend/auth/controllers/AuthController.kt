0b23c9421cd0794ee2aa016d38298cb87bea64d5
Giuseppe Eletto <peppe.eletto@gmail.com>
2022-02-06 15:15:08 +0100

    Set explicit response type

--- auth/src/main/kotlin/brc/studybuddy/backend/auth/controllers/AuthController.kt
+++ auth/src/main/kotlin/brc/studybuddy/backend/auth/controllers/AuthController.kt
@@ -1,6 +1,7 @@
 package brc.studybuddy.backend.auth.controllers
 
 import brc.studybuddy.backend.auth.models.LoginResponse
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -8,11 +9,11 @@ import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping(value = ["/auth"], produces = [MediaType.APPLICATION_JSON_VALUE])
 class AuthController {
 
     @GetMapping
     fun handleGet(): Mono<LoginResponse> =
-         Mono.just(LoginResponse(true, "j5n3wsxj09j3"))
+        Mono.just(LoginResponse(true, "j5n3wsxj09j3"))
 
 }
