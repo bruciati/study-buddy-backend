60532cc44ca7cfb00d2859f92fc13c6589939fbb
Giuseppe Eletto <peppe.eletto@gmail.com>
2022-02-06 15:21:10 +0100

    Add a more expressive service response error data class

--- auth/src/main/kotlin/brc/studybuddy/backend/auth/models/ServiceResponse.kt
+++ auth/src/main/kotlin/brc/studybuddy/backend/auth/models/ServiceResponse.kt
@@ -1,7 +1,12 @@
 package brc.studybuddy.backend.auth.models
 
 data class ServiceResponse<T>(
-        val status: Boolean,
-        val data: T?,
-        val error: Exception?
-    )
+    val status: Boolean,
+    val data: T?,
+    val error: ServiceError?
+)
+
+data class ServiceError(
+    val code: Int,
+    val message: String,
+)
