81370f938ace92fe779e6aa67d970d7f70238d45
Giuseppe Eletto <peppe.eletto@gmail.com>
2022-02-11 20:58:58 +0100

    Add example error endpoint

--- template/src/main/kotlin/brc/studybuddy/backend/template/controller/TemplateController.kt
+++ template/src/main/kotlin/brc/studybuddy/backend/template/controller/TemplateController.kt
@@ -3,14 +3,19 @@ package brc.studybuddy.backend.template.controller
 import brc.studybuddy.model.LoginType
 import brc.studybuddy.model.User
 import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping(value = ["/template"], produces = [MediaType.APPLICATION_JSON_VALUE])
 class TemplateController {
     @GetMapping
     fun defaultHandler(): Mono<User> = Mono.just(User(1u, "test@email.com", LoginType.PASSWORD, "sha256::pwd123", 0u))
+
+    @GetMapping("/error")
+    fun errorHandler(): Mono<User> = Mono.error(Exception("It's a good error! :D"))
 }
