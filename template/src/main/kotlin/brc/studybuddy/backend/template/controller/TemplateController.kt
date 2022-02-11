c70da5b0fb7f2a936ddde0198558b979912eafa8
Giuseppe Eletto <peppe.eletto@gmail.com>
2022-02-11 22:33:37 +0100

    Update template controller

--- template/src/main/kotlin/brc/studybuddy/backend/template/controller/TemplateController.kt
+++ template/src/main/kotlin/brc/studybuddy/backend/template/controller/TemplateController.kt
@@ -7,15 +7,28 @@ import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 @RestController
 @CrossOrigin("*")
-@RequestMapping(value = ["/template"], produces = [MediaType.APPLICATION_JSON_VALUE])
+@RequestMapping(value = ["template"], produces = [MediaType.APPLICATION_JSON_VALUE])
 class TemplateController {
     @GetMapping
     fun defaultHandler(): Mono<User> = Mono.just(User(1u, "test@email.com", LoginType.PASSWORD, "sha256::pwd123", 0u))
 
-    @GetMapping("/error")
+    @GetMapping("flux")
+    fun fluxHandler(): Flux<User> = Flux.fromArray(
+        arrayOf(
+            User(1u, "test1@email.com", LoginType.PASSWORD, "sha256::pwd123", 0u),
+            User(2u, "test2@email.com", LoginType.FACEBOOK, "sha256::pwd123", 4u),
+            User(3u, "test3@email.com", LoginType.PASSWORD, "sha256::pwd123", 7u),
+            User(4u, "test4@email.com", LoginType.FACEBOOK, "sha256::pwd123", 9u),
+            User(5u, "test5@email.com", LoginType.FACEBOOK, "sha256::pwd123", 0u),
+            User(6u, "test6@email.com", LoginType.PASSWORD, "sha256::pwd123", 1u)
+        )
+    )
+
+    @GetMapping("error")
     fun errorHandler(): Mono<User> = Mono.error(Exception("It's a good error! :D"))
 }
