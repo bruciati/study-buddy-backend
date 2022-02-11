0a8e35ae4a49ec5208ca9352a3b20b29f4657379
Giuseppe Eletto <peppe.eletto@gmail.com>
2022-02-11 23:08:44 +0100

    Move Response/Exception wrapper into "StudyBuddy Utils (backend)"

--- template/src/main/kotlin/brc/studybuddy/backend/template/MicroserviceApplication.kt
+++ template/src/main/kotlin/brc/studybuddy/backend/template/MicroserviceApplication.kt
@@ -1,13 +1,19 @@
 package brc.studybuddy.backend.template
 
-import brc.studybuddy.backend.template.wrapper.ResponseWrapper
+import brc.studybuddy.backend.wrapper.ResponseWrapper
+import brc.studybuddy.backend.wrapper.WebExceptionWrapper
+import org.springframework.beans.factory.ObjectProvider
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.web.WebProperties
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.reactive.error.ErrorAttributes
+import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.Bean
+import org.springframework.core.annotation.Order
 import org.springframework.http.codec.ServerCodecConfigurer
 import org.springframework.web.reactive.accept.RequestedContentTypeResolver
+import org.springframework.web.reactive.result.view.ViewResolver
 
 @SpringBootApplication
 class MicroserviceApplication {
@@ -17,13 +23,27 @@ class MicroserviceApplication {
     @Autowired
     lateinit var requestedContentTypeResolver: RequestedContentTypeResolver
 
+    @Autowired
+    lateinit var viewResolvers: ObjectProvider<ViewResolver>
+
+    @Autowired
+    lateinit var errorAttributes: ErrorAttributes
+
+    @Autowired
+    lateinit var applicationContext: ApplicationContext
+
+    val resources: WebProperties.Resources = WebProperties.Resources()
+
     @Bean
     fun responseWrapper(): ResponseWrapper = ResponseWrapper(
         serverCodecConfigurer.writers, requestedContentTypeResolver
     )
 
     @Bean
-    fun resources(): WebProperties.Resources = WebProperties.Resources()
+    @Order(-2)
+    fun webExceptionWrapper(): WebExceptionWrapper = WebExceptionWrapper(
+        viewResolvers, serverCodecConfigurer, errorAttributes, resources, applicationContext
+    )
 }
 
 fun main(args: Array<String>) {
