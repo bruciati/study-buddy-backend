e29470538603fbe52760c4832bae8dcadafb2f91
Giuseppe Eletto <peppe.eletto@gmail.com>
2022-02-11 20:58:34 +0100

    Remove useless class

--- template/src/main/kotlin/brc/studybuddy/backend/template/ServletInitializer.kt
+++
@@ -1,9 +0,0 @@
-package brc.studybuddy.backend.template
-
-import org.springframework.boot.builder.SpringApplicationBuilder
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
-
-class ServletInitializer : SpringBootServletInitializer() {
-    override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder =
-        application.sources(MicroserviceApplication::class.java)
-}
