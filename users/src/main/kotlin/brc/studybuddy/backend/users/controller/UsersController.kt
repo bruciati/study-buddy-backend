3d2f1a2cb431446dc3dfcf99ae19ddda5681a6a8
Giuseppe Eletto <peppe.eletto@gmail.com>
2022-02-14 19:02:05 +0100

    Remove useless imports

--- users/src/main/kotlin/brc/studybuddy/backend/users/controller/UsersController.kt
+++ users/src/main/kotlin/brc/studybuddy/backend/users/controller/UsersController.kt
@@ -3,7 +3,6 @@ package brc.studybuddy.backend.users.controller
 import brc.studybuddy.backend.users.repository.UserRepository
 import brc.studybuddy.model.User
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.stereotype.Controller
 import reactor.core.publisher.Flux
