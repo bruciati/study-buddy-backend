0c1410bf29e2d1d19e6f323bc1b138a4c2f654c9
Giuseppe Eletto <peppe.eletto@gmail.com>
2022-02-15 14:36:33 +0100

    Update controller types

--- template/src/main/kotlin/brc/studybuddy/backend/template/controller/TemplateController.kt
+++ template/src/main/kotlin/brc/studybuddy/backend/template/controller/TemplateController.kt
@@ -13,17 +13,17 @@ import reactor.core.publisher.Mono
 @RequestMapping(value = ["template"], produces = [MediaType.APPLICATION_JSON_VALUE])
 class TemplateController {
     @GetMapping
-    fun defaultHandler(): Mono<User> = Mono.just(User(1u, "test@email.com", LoginType.PASSWORD, "sha256::pwd123", 0u))
+    fun defaultHandler(): Mono<User> = Mono.just(User(1, "test@email.com", LoginType.PASSWORD, "sha256::pwd123", 0))
 
     @GetMapping("flux")
     fun fluxHandler(): Flux<User> = Flux.fromArray(
         arrayOf(
-            User(1u, "test1@email.com", LoginType.PASSWORD, "sha256::pwd123", 0u),
-            User(2u, "test2@email.com", LoginType.FACEBOOK, "sha256::pwd123", 4u),
-            User(3u, "test3@email.com", LoginType.PASSWORD, "sha256::pwd123", 7u),
-            User(4u, "test4@email.com", LoginType.FACEBOOK, "sha256::pwd123", 9u),
-            User(5u, "test5@email.com", LoginType.FACEBOOK, "sha256::pwd123", 0u),
-            User(6u, "test6@email.com", LoginType.PASSWORD, "sha256::pwd123", 1u)
+            User(1, "test1@email.com", LoginType.PASSWORD, "sha256::pwd123", 0),
+            User(2, "test2@email.com", LoginType.FACEBOOK, "sha256::pwd123", 4),
+            User(3, "test3@email.com", LoginType.PASSWORD, "sha256::pwd123", 7),
+            User(4, "test4@email.com", LoginType.FACEBOOK, "sha256::pwd123", 9),
+            User(5, "test5@email.com", LoginType.FACEBOOK, "sha256::pwd123", 0),
+            User(6, "test6@email.com", LoginType.PASSWORD, "sha256::pwd123", 1)
         )
     )
 
