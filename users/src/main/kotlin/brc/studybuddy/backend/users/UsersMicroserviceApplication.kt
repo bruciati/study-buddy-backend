2ce16853cc5b4c8dd9ea582673a0adf884998cfe
Giuseppe Eletto <peppe.eletto@gmail.com>
2022-02-15 18:29:22 +0100

    Update microservice based on the updated lib

--- users/src/main/kotlin/brc/studybuddy/backend/users/UsersMicroserviceApplication.kt
+++ users/src/main/kotlin/brc/studybuddy/backend/users/UsersMicroserviceApplication.kt
@@ -1,50 +1,11 @@
 package brc.studybuddy.backend.users
 
-import brc.studybuddy.backend.wrapper.ResponseWrapper
-import brc.studybuddy.backend.wrapper.WebExceptionWrapper
-import org.springframework.beans.factory.ObjectProvider
-import org.springframework.beans.factory.annotation.Autowired
+import brc.studybuddy.backend.wrapper.WrapperConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.web.WebProperties
 import org.springframework.boot.runApplication
-import org.springframework.boot.web.reactive.error.ErrorAttributes
-import org.springframework.context.ApplicationContext
-import org.springframework.context.annotation.Bean
-import org.springframework.core.annotation.Order
-import org.springframework.http.codec.ServerCodecConfigurer
-import org.springframework.web.reactive.accept.RequestedContentTypeResolver
-import org.springframework.web.reactive.result.view.ViewResolver
 
 @SpringBootApplication
-class UsersMicroserviceApplication {
-    @Autowired
-    lateinit var serverCodecConfigurer: ServerCodecConfigurer
-
-    @Autowired
-    lateinit var requestedContentTypeResolver: RequestedContentTypeResolver
-
-    @Autowired
-    lateinit var viewResolvers: ObjectProvider<ViewResolver>
-
-    @Autowired
-    lateinit var errorAttributes: ErrorAttributes
-
-    @Autowired
-    lateinit var applicationContext: ApplicationContext
-
-    val resources: WebProperties.Resources = WebProperties.Resources()
-
-    @Bean
-    fun responseWrapper(): ResponseWrapper = ResponseWrapper(
-        serverCodecConfigurer.writers, requestedContentTypeResolver
-    )
-
-    @Bean
-    @Order(-2)
-    fun webExceptionWrapper(): WebExceptionWrapper = WebExceptionWrapper(
-        viewResolvers, serverCodecConfigurer, errorAttributes, resources, applicationContext
-    )
-}
+class UsersMicroserviceApplication : WrapperConfiguration()
 
 fun main(args: Array<String>) {
     runApplication<UsersMicroserviceApplication>(*args)
