1289d6d8ea7adf6740360c0a3b7d2e506424953d
Ollenurb <brunello.mtt@gmail.com>
2022-02-23 18:50:38 +0100

    Added Eureka client annotation

--- gateway/src/main/kotlin/brc/studybuddy/backend/gateway/GatewayMicroserviceApplication.kt
+++ gateway/src/main/kotlin/brc/studybuddy/backend/gateway/GatewayMicroserviceApplication.kt
@@ -2,7 +2,9 @@ package brc.studybuddy.backend.gateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
+@EnableEurekaClient
 @SpringBootApplication
 class GatewayMicroserviceApplication
 
