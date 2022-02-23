cdcb39b760497b06ac62ca705a65a079bb38547f
Giuseppe Eletto <peppe.eletto@gmail.com>
2022-02-23 20:46:45 +0100

    Files "pom.xml" and "application.yml" refactored

--- groups/src/main/kotlin/brc/studybuddy/backend/groups/GroupsMicroserviceApplication.kt
+++ groups/src/main/kotlin/brc/studybuddy/backend/groups/GroupsMicroserviceApplication.kt
@@ -1,23 +1,12 @@
 package brc.studybuddy.backend.groups
 
-import com.rabbitmq.client.Connection
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import reactor.core.publisher.Mono
-import javax.annotation.PreDestroy
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
+@EnableEurekaClient
 @SpringBootApplication
-class GroupsMicroserviceApplication {
-    @Autowired
-    private lateinit var rabbitConnection: Mono<Connection>
-
-    @PreDestroy
-    @Throws(Exception::class)
-    fun close() {
-        rabbitConnection.block()!!.close()
-    }
-}
+class GroupsMicroserviceApplication
 
 fun main(args: Array<String>) {
     runApplication<GroupsMicroserviceApplication>(*args)
