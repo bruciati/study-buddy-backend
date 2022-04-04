062d596782a9a9ccf929bc9c3a41817888ccf935
Giuseppe Eletto <peppe.eletto@gmail.com>
2022-04-04 18:26:10 +0200

    Group refactoring (WIP)

--- groups/src/main/kotlin/brc/studybuddy/backend/groups/graphql/MutationTypeController.kt
+++ groups/src/main/kotlin/brc/studybuddy/backend/groups/graphql/MutationTypeController.kt
@@ -1,57 +1,12 @@
 package brc.studybuddy.backend.groups.graphql
 
-import brc.studybuddy.backend.groups.model.GroupInput
-import brc.studybuddy.backend.groups.repository.GroupMembersRepository
 import brc.studybuddy.backend.groups.repository.GroupsRepository
-import brc.studybuddy.model.Group
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.dao.DataIntegrityViolationException
-import org.springframework.graphql.data.method.annotation.Argument
-import org.springframework.graphql.data.method.annotation.MutationMapping
 import org.springframework.stereotype.Controller
-import org.springframework.transaction.annotation.Transactional
-import reactor.core.publisher.Mono
 
 @Controller
 class MutationTypeController {
     @Autowired
     private lateinit var groupsRepository: GroupsRepository
 
-    @Autowired
-    private lateinit var groupMembersRepository: GroupMembersRepository
-
-    // NOTE it won't check if the user exists
-    @Transactional
-    @MutationMapping
-    fun addGroup(
-        @Argument input: GroupInput
-    ): Mono<Group> = Mono.just(input.toGroup())
-        .flatMap(groupsRepository::save)
-        .flatMap { g ->
-            val member = input.toGroupMember(g.id, true)
-            groupMembersRepository.save(member).thenReturn(g)
-        }
-        .onErrorMap { e ->
-            when (e) {
-                is NullPointerException -> Exception("All required parameters (\"owner\", \"title\") must be defined!")
-                is DataIntegrityViolationException -> Exception("A group with the given title already exists!")
-                else -> e
-            }
-        }
-
-    // NOTE owner change will not be implemented
-    @MutationMapping
-    fun updateGroup(
-        @Argument id: Long,
-        @Argument input: GroupInput
-    ): Mono<Group> = groupsRepository.findById(id)
-        .map(input::updateGroup)
-        .flatMap(groupsRepository::save)
-
-    // TODO delete meetings
-    @MutationMapping
-    fun deleteGroup(
-        @Argument id: Long
-    ): Mono<Boolean> = groupsRepository.deleteById(id)
-        .thenReturn(true) // Can not be 'false'
 }
--- groups/src/main/kotlin/brc/studybuddy/backend/groups/graphql/QueryTypeController.kt
+++ groups/src/main/kotlin/brc/studybuddy/backend/groups/graphql/QueryTypeController.kt
@@ -1,14 +1,11 @@
 package brc.studybuddy.backend.groups.graphql
 
-import brc.studybuddy.backend.groups.model.GroupMember
-import brc.studybuddy.backend.groups.repository.GroupMembersRepository
 import brc.studybuddy.backend.groups.repository.GroupsRepository
 import brc.studybuddy.model.Group
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.stereotype.Controller
-import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
@@ -17,30 +14,10 @@ class QueryTypeController {
     @Autowired
     private lateinit var groupsRepository: GroupsRepository
 
-    @Autowired
-    private lateinit var groupMembersRepository: GroupMembersRepository
-
-    @Autowired
-    private lateinit var webClientBuilder: WebClient.Builder
-
-    private val usersWebClient: WebClient by lazy { webClientBuilder.baseUrl("lb://users/users").build() }
-
     @QueryMapping
     fun groups(@Argument ids: List<Long>?): Flux<Group> = Mono.justOrEmpty(ids)
         .flatMapMany(groupsRepository::findAllById)
         .switchIfEmpty(groupsRepository.findAll())
-
-    @QueryMapping
-    fun groupsByOwnerId(@Argument id: Long): Flux<Group> = groupMembersRepository.findAllByUserIdAndIsOwner(id)
-        .map(GroupMember::groupId)
-        .collectList()
-        .flatMapMany(groupsRepository::findAllById)
-
-    @QueryMapping
-    fun groupsByUserId(@Argument id: Long): Flux<Group> = groupMembersRepository.findAllByUserId(id)
-        .map(GroupMember::groupId)
-        .collectList()
-        .flatMapMany(groupsRepository::findAllById)
 
     @QueryMapping
     fun groupById(@Argument id: Long): Mono<Group> = groupsRepository.findById(id)
