bf20d33358b2460f24dcc2fffc51d231638bea46
Giuseppe Eletto <peppe.eletto@gmail.com>
2022-04-06 00:46:43 +0200

    Add WebClient services

--- groups/src/main/kotlin/brc/studybuddy/backend/groups/repository/GroupsRepository.kt
+++ groups/src/main/kotlin/brc/studybuddy/backend/groups/repository/GroupsRepository.kt
@@ -3,7 +3,6 @@ package brc.studybuddy.backend.groups.repository
 import brc.studybuddy.model.Group
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import org.springframework.stereotype.Repository
-import reactor.core.publisher.Mono
 
 @Repository
 sealed interface GroupsRepository : ReactiveCrudRepository<Group, Long>
