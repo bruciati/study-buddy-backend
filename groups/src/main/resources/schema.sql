762e418397881709367c4a6c357a2a2a9dca2b81
Giuseppe Eletto <peppe.eletto@gmail.com>
2022-04-06 00:45:34 +0200

    Prettify SQL schema

--- groups/src/main/resources/schema.sql
+++ groups/src/main/resources/schema.sql
@@ -1,20 +1,22 @@
 --
 -- Table structure for table `groups`
 --
-CREATE TABLE IF NOT EXISTS groups (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(128) UNIQUE NOT NULL,
+CREATE TABLE IF NOT EXISTS groups
+(
+    id          SERIAL PRIMARY KEY,
+    title       VARCHAR(128) UNIQUE NOT NULL,
     description TEXT DEFAULT NULL
 );
 
 --
 -- Table structure for table `members`
 --
-CREATE TABLE IF NOT EXISTS members (
-    group_id SERIAL NOT NULL,
-    user_id SERIAL NOT NULL,
+CREATE TABLE IF NOT EXISTS members
+(
+    group_id SERIAL                NOT NULL,
+    user_id  SERIAL                NOT NULL,
     is_owner BOOLEAN DEFAULT false NOT NULL,
-    FOREIGN KEY (group_id) REFERENCES groups(id)
+    FOREIGN KEY (group_id) REFERENCES groups (id)
         ON DELETE CASCADE
         ON UPDATE CASCADE,
     PRIMARY KEY (user_id, group_id)
