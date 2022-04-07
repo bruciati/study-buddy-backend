3224a5454e71b383688d206fc925fdc501b09e90
Giuseppe Eletto <peppe.eletto@gmail.com>
2022-04-07 19:53:15 +0200

    Update dumps

--- meetings/dump.sql
+++ meetings/dump.sql
@@ -1,48 +1,50 @@
 --
 -- Table structure for table `meetings`
 --
-CREATE TABLE IF NOT EXISTS meetings (
-    id SERIAL PRIMARY KEY,
-    group_id INTEGER NOT NULL,
-    name VARCHAR(128) NOT NULL,
-    datetime BIGINT NOT NULL,
-    type VARCHAR(8) NOT NULL,
-    location VARCHAR(64) NOT NULL,
+CREATE TABLE IF NOT EXISTS meetings
+(
+    id       SERIAL PRIMARY KEY,
+    group_id INTEGER      NOT NULL,
+    name     VARCHAR(128) NOT NULL,
+    datetime BIGINT       NOT NULL,
+    type     VARCHAR(8)   NOT NULL,
+    location VARCHAR(64)  NOT NULL,
+    UNIQUE KEY (group_id, name)
 );
 
 --
 -- Dumping data for table `meetings`
 --
-INSERT INTO meetings VALUES
-    (1, 1, 'Ali della libertà', 11, 'PHYSICAL', 'Torino'),
-    (2, 2, 'A scuola con Paolo', 12, 'PHYSICAL', 'Torino'),
-    (3, 2, 'Ulzio e le sue caprette', 14, 'ONLINE', 'Torino'),
-    (4, 3, 'Giovani autistici', 16, 'ONLINE', 'Torino');
+INSERT INTO meetings
+VALUES (1, 1, 'Ali della libertà', 11, 'PHYSICAL', 'Torino'),
+       (2, 2, 'A scuola con Paolo', 12, 'PHYSICAL', 'Torino'),
+       (3, 2, 'Ulzio e le sue caprette', 14, 'ONLINE', 'Torino'),
+       (4, 3, 'Giovani autistici', 16, 'ONLINE', 'Torino');
 
 --
 -- Table structure for table `attendees`
 --
-CREATE TABLE IF NOT EXISTS attendees (
-    meeting_id SERIAL NOT NULL,
-    user_id SERIAL NOT NULL,
-    is_host BOOLEAN DEFAULT false NOT NULL,
-    FOREIGN KEY (meeting_id) REFERENCES meetings(id)
+CREATE TABLE IF NOT EXISTS attendees
+(
+    meeting_id INTEGER               NOT NULL,
+    user_id    INTEGER               NOT NULL,
+    is_host    BOOLEAN DEFAULT false NOT NULL,
+    PRIMARY KEY (user_id, meeting_id),
+    FOREIGN KEY (meeting_id) REFERENCES meetings (id)
         ON DELETE CASCADE
-        ON UPDATE CASCADE,
-    PRIMARY KEY (user_id, meeting_id)
+        ON UPDATE CASCADE
 );
 
 --
 -- Dumping data for table `attendees`
 --
-INSERT INTO attendees VALUES
-    (1, 1,  true),
-    (2, 3,  true),
-    (3, 2,  true),
-    (1, 2,  false),
-    (1, 3,  false),
-    (2, 1,  false),
-    (2, 2,  false),
-    (3, 1,  false),
-    (3, 2,  false);
-
+INSERT INTO attendees
+VALUES (1, 1, true),
+       (2, 3, true),
+       (3, 2, true),
+       (1, 2, false),
+       (1, 3, false),
+       (2, 1, false),
+       (2, 2, false),
+       (3, 1, false),
+       (3, 3, false);
--- users/dump.sql
+++ users/dump.sql
@@ -1,79 +1,82 @@
 --
 -- Table structure for table `users`
 --
-CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    email VARCHAR(128) UNIQUE NOT NULL,
-    auth_type VARCHAR(8) NOT NULL,
-    auth_value VARCHAR(256) NOT NULL
+CREATE TABLE IF NOT EXISTS users
+(
+    id         SERIAL PRIMARY KEY,
+    email      VARCHAR(128) UNIQUE NOT NULL,
+    auth_type  VARCHAR(8)          NOT NULL,
+    auth_value VARCHAR(256)        NOT NULL
 );
 
 --
 -- Dumping data for table `users`
 --
-INSERT INTO users VALUES
-    (1, 'matteo.brunello@edu.unito.it', 'PASSWORD', 'pwd123'),
-    (2, 'giuseppe.eletto@edu.unito.it', 'PASSWORD', 'pwd123'),
-    (3, 'edoardo.chiavazza@edu.unito.it', 'FACEBOOK', 'token123');
-    
+INSERT INTO users
+VALUES (1, 'matteo.brunello@edu.unito.it', 'PASSWORD', 'pwd123'),
+       (2, 'giuseppe.eletto@edu.unito.it', 'PASSWORD', 'pwd123'),
+       (3, 'edoardo.chiavazza@edu.unito.it', 'FACEBOOK', 'token123');
+
 --
 -- Table structure for table `members`
 --
-CREATE TABLE IF NOT EXISTS members (
-    group_id SERIAL NOT NULL,
-    user_id SERIAL NOT NULL,
+CREATE TABLE IF NOT EXISTS members
+(
+    group_id INTEGER               NOT NULL,
+    user_id  INTEGER               NOT NULL,
     is_owner BOOLEAN DEFAULT false NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    PRIMARY KEY (user_id, group_id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
         ON DELETE CASCADE
-        ON UPDATE CASCADE,
-    PRIMARY KEY (user_id, group_id)
+        ON UPDATE CASCADE
 );
 
 --
 -- Dumping data for table `members`
 --
-INSERT INTO members VALUES
-    (4, 1,  true),
-    (3, 1,  true),
-    (5, 2,  true),
-    (1, 2,  true),
-    (6, 2,  true),
-    (2, 3,  true),
-    (1, 1,  false),
-    (3, 2,  false),
-    (5, 3,  false),
-    (2, 1,  false),
-    (4, 2,  false),
-    (3, 3,  false),
-    (6, 1,  false),
-    (2, 2,  false),
-    (6, 3,  false),
-    (5, 1,  false),
-    (4, 3,  false);
+INSERT INTO members
+VALUES (4, 1, true),
+       (3, 1, true),
+       (5, 2, true),
+       (1, 2, true),
+       (6, 2, true),
+       (2, 3, true),
+       (1, 1, false),
+       (3, 2, false),
+       (5, 3, false),
+       (2, 1, false),
+       (4, 2, false),
+       (3, 3, false),
+       (6, 1, false),
+       (2, 2, false),
+       (6, 3, false),
+       (5, 1, false),
+       (4, 3, false);
 
 --
 -- Table structure for table `attendees`
 --
-CREATE TABLE IF NOT EXISTS attendees (
-    meeting_id SERIAL NOT NULL,
-    user_id SERIAL NOT NULL,
-    is_host BOOLEAN DEFAULT false NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id)
+CREATE TABLE IF NOT EXISTS attendees
+(
+    meeting_id INTEGER               NOT NULL,
+    user_id    INTEGER               NOT NULL,
+    is_host    BOOLEAN DEFAULT false NOT NULL,
+    PRIMARY KEY (user_id, meeting_id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
         ON DELETE CASCADE
-        ON UPDATE CASCADE,
-    PRIMARY KEY (user_id, meeting_id)
+        ON UPDATE CASCADE
 );
 
 --
 -- Dumping data for table `attendees`
 --
-INSERT INTO attendees VALUES
-    (1, 1,  true),
-    (2, 3,  true),
-    (3, 2,  true),
-    (1, 2,  false),
-    (1, 3,  false),
-    (2, 1,  false),
-    (2, 2,  false),
-    (3, 1,  false),
-    (3, 3,  false);
+INSERT INTO attendees
+VALUES (1, 1, true),
+       (2, 3, true),
+       (3, 2, true),
+       (1, 2, false),
+       (1, 3, false),
+       (2, 1, false),
+       (2, 2, false),
+       (3, 1, false),
+       (3, 3, false);
