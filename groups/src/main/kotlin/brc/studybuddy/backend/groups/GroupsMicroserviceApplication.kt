b7ca90d846a397b08dc92a4e067d90009619498e
Giuseppe Eletto <peppe.eletto@gmail.com>
2022-04-07 19:35:55 +0200

    Remove redundant bean declaration

--- groups/src/main/kotlin/brc/studybuddy/backend/groups/GroupsMicroserviceApplication.kt
+++ groups/src/main/kotlin/brc/studybuddy/backend/groups/GroupsMicroserviceApplication.kt
@@ -1,28 +1,12 @@
 package brc.studybuddy.backend.groups
 
-import io.r2dbc.spi.ConnectionFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
-import org.springframework.context.annotation.Bean
-import org.springframework.core.io.ClassPathResource
-import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer
-import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator
 
 @EnableEurekaClient
 @SpringBootApplication
-class GroupsMicroserviceApplication {
-
-    @Bean
-    @Suppress("SpringJavaInjectionPointsAutowiringInspection")
-    fun dbConnInitializer(connectionFactory: ConnectionFactory): ConnectionFactoryInitializer =
-        with(ConnectionFactoryInitializer()) {
-            this.setConnectionFactory(connectionFactory)
-            this.setDatabasePopulator(ResourceDatabasePopulator(ClassPathResource("schema.sql")))
-            return this
-        }
-
-}
+class GroupsMicroserviceApplication
 
 fun main(args: Array<String>) {
     runApplication<GroupsMicroserviceApplication>(*args)
