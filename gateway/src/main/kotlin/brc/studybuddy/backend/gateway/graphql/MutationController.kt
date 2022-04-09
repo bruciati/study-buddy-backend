89676e8c0568a33d4ab7cce16b946300759a7f56
Giuseppe Eletto <peppe.eletto@gmail.com>
2022-04-09 14:33:53 +0200

    Add GraphQl mutation methods (not implemented)

--- gateway/src/main/kotlin/brc/studybuddy/backend/gateway/graphql/MutationController.kt
+++ gateway/src/main/kotlin/brc/studybuddy/backend/gateway/graphql/MutationController.kt
@@ -3,9 +3,16 @@ package brc.studybuddy.backend.gateway.graphql
 import brc.studybuddy.backend.gateway.client.GroupsWebClient
 import brc.studybuddy.backend.gateway.client.MeetingsWebClient
 import brc.studybuddy.backend.gateway.client.UsersWebClient
+import brc.studybuddy.input.GroupInput
+import brc.studybuddy.input.MeetingInput
+import brc.studybuddy.model.Group
+import brc.studybuddy.model.Meeting
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.graphql.data.method.annotation.Argument
+import org.springframework.stereotype.Controller
+import reactor.core.publisher.Mono
 
-//@Controller
+@Controller
 class MutationController {
     @Autowired
     private lateinit var usersWebClient: UsersWebClient
@@ -17,5 +24,27 @@ class MutationController {
     private lateinit var meetingsWebClient: MeetingsWebClient
 
 
+    // ------------------------------------------------------
+    // --------------------- Group Class --------------------
+    // ------------------------------------------------------
 
+    fun saveGroup(@Argument input: GroupInput): Mono<Group> = TODO()
+
+    fun updateGroup(@Argument input: GroupInput): Mono<Group> = TODO()
+
+    fun deleteGroup(@Argument id: Long): Mono<Boolean> = TODO()
+
+    fun addGroupMember(@Argument groupId: Long, @Argument userId: Long): Mono<Boolean> = TODO()
+
+    // ------------------------------------------------------
+    // -------------------- Meeting Class -------------------
+    // ------------------------------------------------------
+
+    fun saveMeeting(@Argument input: MeetingInput): Mono<Meeting> = TODO()
+
+    fun updateMeeting(@Argument input: MeetingInput): Mono<Meeting> = TODO()
+
+    fun deleteMeeting(@Argument id: Long): Mono<Boolean> = TODO()
+
+    fun addMeetingAttendee(@Argument meetingId: Long, @Argument userId: Long): Mono<Boolean> = TODO()
 }
