7e5a4e668b434e2e08d04179099407d7eb6dd4aa
Giuseppe Eletto <peppe.eletto@gmail.com>
2022-04-09 15:27:28 +0200

    Update WebRequestFilter class

--- gateway/src/main/kotlin/brc/studybuddy/backend/gateway/auth/WebRequestFilter.kt
+++ gateway/src/main/kotlin/brc/studybuddy/backend/gateway/auth/WebRequestFilter.kt
@@ -1,18 +1,31 @@
 package brc.studybuddy.backend.gateway.auth
 
+import brc.studybuddy.backend.gateway.config.AuthConfig
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
-import org.springframework.web.reactive.function.server.HandlerFilterFunction
-import org.springframework.web.reactive.function.server.HandlerFunction
-import org.springframework.web.reactive.function.server.ServerRequest
-import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.server.ServerWebExchange
+import org.springframework.web.server.WebFilter
+import org.springframework.web.server.WebFilterChain
 import reactor.core.publisher.Mono
 
 @Component
-class WebRequestFilter : HandlerFilterFunction<ServerResponse, ServerResponse> {
+class WebRequestFilter : WebFilter {
+    @Autowired
+    lateinit var authConfig: AuthConfig
 
-    override fun filter(request: ServerRequest, next: HandlerFunction<ServerResponse>): Mono<ServerResponse> {
+    private fun isClientAuthorized(exchange: ServerWebExchange, chain: WebFilterChain): Boolean {
         // TODO Implement unauthenticated request filtering
-        return next.handle(request);
+        return true
     }
 
+    override fun filter(exchange: ServerWebExchange, chain: WebFilterChain): Mono<Void> {
+        val pathString = exchange.request.path.value()
+
+        if (!pathString.startsWith("/auth"))
+            if (!isClientAuthorized(exchange, chain))
+                exchange.response.statusCode = HttpStatus.UNAUTHORIZED
+
+        return chain.filter(exchange)
+    }
 }
