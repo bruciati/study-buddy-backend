32c6a5366a2332d9ef97c8668b52ecd112ca1e72
Giuseppe Eletto <peppe.eletto@gmail.com>
2022-04-09 19:52:49 +0200

    Update authorization filter (unimplemented)

--- gateway/src/main/kotlin/brc/studybuddy/backend/gateway/auth/WebRequestFilter.kt
+++ gateway/src/main/kotlin/brc/studybuddy/backend/gateway/auth/WebRequestFilter.kt
@@ -2,6 +2,7 @@ package brc.studybuddy.backend.gateway.auth
 
 import brc.studybuddy.backend.gateway.config.AuthConfig
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.core.annotation.Order
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
 import org.springframework.web.server.ServerWebExchange
@@ -10,22 +11,29 @@ import org.springframework.web.server.WebFilterChain
 import reactor.core.publisher.Mono
 
 @Component
+@Order(Int.MIN_VALUE)
 class WebRequestFilter : WebFilter {
     @Autowired
     lateinit var authConfig: AuthConfig
 
-    private fun isClientAuthorized(exchange: ServerWebExchange, chain: WebFilterChain): Boolean {
+
+    private fun isClientAuthorized(exchange: ServerWebExchange): Boolean {
+        val authCode = exchange.request.headers.getFirst("Authorization")
+
         // TODO Implement unauthenticated request filtering
         return true
     }
 
+
+    // Webflux filter
     override fun filter(exchange: ServerWebExchange, chain: WebFilterChain): Mono<Void> {
-        val pathString = exchange.request.path.value()
-
-        if (!pathString.startsWith("/auth"))
-            if (!isClientAuthorized(exchange, chain))
+        val reqPath = exchange.request.path.value()
+        return when (reqPath.startsWith("/auth") || isClientAuthorized(exchange)) {
+            true -> chain.filter(exchange)
+            false -> {
                 exchange.response.statusCode = HttpStatus.UNAUTHORIZED
-
-        return chain.filter(exchange)
+                exchange.response.setComplete()
+            }
+        }
     }
 }
