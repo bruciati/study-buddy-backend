50eaa1d20a50e38aab1db043ae6f32c5d5a816db
Giuseppe Eletto <peppe.eletto@gmail.com>
2022-04-15 17:53:13 +0200

    Enable the Config server along with the Eureka one

--- eureka/src/main/kotlin/brc/studybuddy/backend/eureka/EurekaMicroserviceApplication.kt
+++ eureka/src/main/kotlin/brc/studybuddy/backend/eureka/EurekaMicroserviceApplication.kt
@@ -1,9 +1,12 @@
 package brc.studybuddy.backend.eureka
+
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.config.server.EnableConfigServer
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 @EnableEurekaServer
+@EnableConfigServer
 @SpringBootApplication
 class EurekaMicroserviceApplication
 
