398c82a50e586770f9688d962e0efa691bd0669c
Giuseppe Eletto <peppe.eletto@gmail.com>
2022-09-12 11:17:32 +0200

    Update Gateway CORS configs

---
+++ gateway/src/main/kotlin/brc/studybuddy/backend/gateway/config/CorsConfig.kt
@@ -0,0 +1,31 @@
+package brc.studybuddy.backend.gateway.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.reactive.CorsWebFilter
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
+
+
+@Configuration
+class CorsConfig : CorsConfiguration() {
+
+    @Bean
+    fun corsFilter(): CorsWebFilter {
+        val corsConfiguration = CorsConfiguration()
+        corsConfiguration.allowCredentials = true
+        corsConfiguration.addAllowedOrigin("*")
+        corsConfiguration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD")
+        corsConfiguration.addAllowedHeader("origin")
+        corsConfiguration.addAllowedHeader("content-type")
+        corsConfiguration.addAllowedHeader("accept")
+        corsConfiguration.addAllowedHeader("authorization")
+        corsConfiguration.addAllowedHeader("cookie")
+
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", corsConfiguration)
+
+        return CorsWebFilter(source)
+    }
+
+}
