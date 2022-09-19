164cf33b0219bf2ea3ab41fedc386fa244cb327a
Giuseppe Eletto <peppe.eletto@gmail.com>
2022-09-19 16:36:56 +0200

    Update Users microservice

--- users/src/main/kotlin/brc/studybuddy/backend/users/repository/UsersRepository.kt
+++ users/src/main/kotlin/brc/studybuddy/backend/users/repository/UsersRepository.kt
@@ -1,6 +1,7 @@
 package brc.studybuddy.backend.users.repository
 
 import brc.studybuddy.model.User
+import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Mono
@@ -8,6 +9,9 @@ import reactor.core.publisher.Mono
 @Repository
 sealed interface UsersRepository : ReactiveCrudRepository<User, Long> {
     fun findByEmail(email: String): Mono<User>
+
+    @Query("SELECT * FROM users WHERE auth_type = 'FACEBOOK' AND auth_value = ?")
+    fun findByFbId(id: String): Mono<User>
 
     fun deleteByEmail(email: String): Mono<Void>
 }
