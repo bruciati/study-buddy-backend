d20bf27d25524e0a4c5c5e00d22433634e8e994f
Ollenurb <brunello.mtt@gmail.com>
2022-09-19 17:35:07 +0200

    Fixed fbid

--- users/src/main/kotlin/brc/studybuddy/backend/users/repository/UsersRepository.kt
+++ users/src/main/kotlin/brc/studybuddy/backend/users/repository/UsersRepository.kt
@@ -2,7 +2,6 @@ package brc.studybuddy.backend.users.repository
 
 import brc.studybuddy.model.User
 import org.springframework.data.r2dbc.repository.Query
-import org.springframework.data.repository.query.Param
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Mono
@@ -11,8 +10,8 @@ import reactor.core.publisher.Mono
 sealed interface UsersRepository : ReactiveCrudRepository<User, Long> {
     fun findByEmail(email: String): Mono<User>
 
-    @Query("SELECT u.* FROM users u WHERE u.auth_type = \"FACEBOOK\" AND u.auth_value = :id")
-    fun findByFbId(@Param("id") id: String): Mono<User>
+    @Query("SELECT u.* FROM users u WHERE u.auth_type = 'FACEBOOK' AND u.auth_value = :id")
+    fun findByFbId(id: String): Mono<User>
 
     fun deleteByEmail(email: String): Mono<Void>
 }
