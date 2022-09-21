aae9e5b14e050e1ecd57396f4db25b4d4d0de395
Giuseppe Eletto <peppe.eletto@gmail.com>
2022-09-21 21:11:04 +0200

    Update Auth microservice

--- auth/src/main/kotlin/brc/studybuddy/backend/auth/controller/AuthController.kt
+++ auth/src/main/kotlin/brc/studybuddy/backend/auth/controller/AuthController.kt
@@ -7,9 +7,8 @@ import brc.studybuddy.backend.auth.model.EmailCredentials
 import brc.studybuddy.backend.auth.service.AuthService
 import brc.studybuddy.backend.auth.service.RegisterService
 import brc.studybuddy.input.UserInput
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
@@ -23,25 +22,30 @@ import reactor.core.publisher.Mono
 class AuthController {
     @Autowired
     lateinit var authService: AuthService
+
     @Autowired
     lateinit var registerService: RegisterService
 
-    val logger: Logger = LoggerFactory.getLogger("AuthenticationController")
+    @Value("\${auth.ttl}")
+    private var accessTokenTtl: Short = 0
+
+    @Value("\${auth.refresh.ttl}")
+    private var refreshTokenTtl: Int = 0
 
     @PostMapping
     fun authenticate(@RequestBody credentials: Credentials): Mono<AuthResponse> =
         authService.authenticate(credentials)
-            .map { s -> AuthSuccess(s.first, s.second) }
+            .map { s -> AuthSuccess(s.first, accessTokenTtl, s.second, refreshTokenTtl) }
 
     @PostMapping(path = ["/refresh"])
     fun refresh(@RequestBody refreshToken: String): Mono<AuthResponse> =
         authService.refresh(refreshToken.removeSurrounding("\""))
-            .map { s -> AuthSuccess(s.first, s.second) }
+            .map { s -> AuthSuccess(s.first, accessTokenTtl, s.second, refreshTokenTtl) }
 
     @PutMapping
     fun register(@RequestBody user: UserInput): Mono<AuthResponse> =
         registerService.register(user)
             .flatMap { authService.authenticate(EmailCredentials(it.email, it.authValue)) }
-            .map { s -> AuthSuccess(s.first, s.second) }
+            .map { s -> AuthSuccess(s.first, accessTokenTtl, s.second, refreshTokenTtl) }
 
 }
